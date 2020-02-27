8fe84ea8bb6e80e39dff6aeae2c7c0f13d1a48cf
Giorgio <giopag27@gmail.com>
2020-02-27 20:51:18 +0100

    bson + started controller

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,15 +1,33 @@
 class CardsController < ApplicationController
 
   def index
+    output = []
+    Card.all.map{ |card|
+      output = [{
 
+      }]
+    }
+    output.to_json
+  rescue => e
+    Rails.logger.error("CARD NOT SAVED #{e} #{e.backtrace.join("\n")}")
+    render status: 500, json: {error: e.to_s}
   end
 
   def create
+    Card.create!(
 
+    )
+  rescue => e
+    Rails.logger.error("CARD NOT SAVED #{e} #{e.backtrace.join("\n")}")
+    render status: 500, json: {error: e.to_s}
   end
 
   def show
-
+    card = Card.find(params[:id])
+    
+  rescue => e
+    Rails.logger.error("CARD NOT SAVED #{e} #{e.backtrace.join("\n")}")
+    render status: 500, json: {error: e.to_s}
   end
 
   def update
